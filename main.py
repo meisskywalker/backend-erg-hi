1996b57f92ecbea50773adf3e811d0e0ab58639b
meisskywalker <meisskywalker@proton.me>
2023-07-29 19:20:26 +0700

    add: add cors

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 
 # from app import models
 # from app.db import engine
@@ -13,19 +13,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+origins = ["http://localhost:5173", "https://erg-hi.netlify.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get('/')
 def root():
     return { 'detail': os.environ.get("DB_URI") }
 
-# origins = ["http://localhost:5173", "https://erg-hi.netlify.app"]
-#
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
 
 # models.Base.metadata.create_all(bind=engine)
 #
