b8ae962fe7986bf5ef8196f400993c64f67279f6
meisskywalker <meisskywalker@proton.me>
2023-07-29 14:56:25 +0700

    add: add origin

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from fastapi import FastAPI
+from dotenv import load_dotenv
 import uvicorn
 
+load_dotenv()
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app import models
+from app.db import engine
+from app.routers import product, user, auth, about_us, contact_us
+
 app = FastAPI()
-@app.get("/", tags=["Root"])
-async def read_root():
-  return { 
-    "message": "Welcome to my notes application, use the /docs route to proceed"
-   }
+
+origins = ["http://localhost:5173", "https://erg-hi.netlify.app"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(product.router)
+# app.include_router(image.router)
+app.include_router(user.router)
+# app.include_router(hero.router)
+app.include_router(about_us.router)
+app.include_router(contact_us.router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
